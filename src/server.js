b3f7ebbe0e9b42be59a6513ac7bb464f16595c3d
Rilton Moreira <rilton_moreira@hotmail.com>
2020-06-23 17:35:22 -0300

    Atualização porta

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 
@@ -7,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Servidor online');
 });
