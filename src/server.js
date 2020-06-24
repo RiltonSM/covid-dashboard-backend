eb176511978806db3b392e2d304b9029f04a5ad7
Rilton Moreira <rilton_moreira@hotmail.com>
2020-06-24 19:18:59 -0300

    Atualização 24-06-2020

--- src/server.js
+++ src/server.js
@@ -2,9 +2,20 @@ require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 
+var whitelist = ['http://covid-dashboard-ce.herokuapp.com/', 'https://covid-dashboard-ce.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
 const routes = require('./routes');
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
